6d98ab39337d94944fc9063915470b8ece7ff62a
renegadelhaedu <renegadelha.edu@gmail.com>
2026-03-02 10:00:03 -0300

    first commit

---
+++ exemplo2.py
@@ -0,0 +1,6 @@
+num1 = 12
+num2 = 71
+textinho = 'hoje eu e meus amigos implementamos uma soma em python'
+final = num1 + num2
+quadrado = final * final 
+print(quadrado)
---
+++ teste.py
@@ -0,0 +1,1 @@
+print('tmj meu povo')
